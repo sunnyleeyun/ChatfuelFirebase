29fc2c78573532a61343203271d103b2722ad9ef
Sunny <sunnylee945@hotmail.com>
2017-09-12 23:46:00 +0800

    first commit

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const functions = require('firebase-functions');
+
+
+const questions = [
+	"Do you take initiatives?",
+	"Do you take care of people?",
+	"Do you prefer to be alone?",
+	"Do you prefer to listen than to talk?",
+	"Do you feel rarely tired?"
+]
+
+exports.sixfactorsGetNextQuestion = functions.https.onRequest((request, response) => {
+
+  console.log("sixfactorsGetNextQuestion : " + JSON.stringify(request.query) );
+  
+  // Grab the questionId parameter.
+  const questionId = request.query["questionId"];
+  // Parse the parameter as an integer
+  var questionIndex = parseInt(questionId, 10);
+
+  // Set 0 if it is not a number or increment the question index
+  if( isNaN(questionIndex) ) {
+  	questionIndex = 0;
+  } else {
+  	questionIndex++;
+  }
+
+
+  if( questionIndex >= 0 && questionIndex < questions.length ) {
+
+  	response.json( {
+		"set_attributes": 
+		{
+			"isComplete": false,
+			"questionId": questionIndex,
+			"questionText": questions[ questionIndex ],
+		}
+	});
+
+  } else {
+
+  	response.json( {
+		"set_attributes": 
+		{
+			"isComplete": true,
+			"questionId": questions.length,
+			"questionText": "",
+		}
+	});
+
+  }
+ 
+  
+});
